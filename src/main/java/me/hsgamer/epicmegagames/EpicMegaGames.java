ee1aa929da344f3fe91170680c29421a0a063fb4
HSGamer <huynhqtienvtag@gmail.com>
2022-05-03 13:25:34 +0700

    per instance server list ping

--- src/main/java/me/hsgamer/epicmegagames/EpicMegaGames.java
+++ src/main/java/me/hsgamer/epicmegagames/EpicMegaGames.java
@@ -5,8 +5,17 @@ import me.hsgamer.epicmegagames.config.LobbyConfig;
 import me.hsgamer.epicmegagames.config.MainConfig;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.command.CommandManager;
+import net.minestom.server.entity.Entity;
+import net.minestom.server.entity.Player;
 import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.event.server.ServerListPingEvent;
 import net.minestom.server.extras.bungee.BungeeCordProxy;
+import net.minestom.server.instance.Instance;
+import net.minestom.server.network.player.PlayerConnection;
+import net.minestom.server.ping.ResponseData;
+
+import java.util.Collection;
+import java.util.Optional;
 
 public class EpicMegaGames {
     private static final MainConfig mainConfig = new MainConfig();
@@ -22,12 +31,26 @@ public class EpicMegaGames {
 
         registerCommands();
 
-        MinecraftServer.getGlobalEventHandler().addListener(PlayerLoginEvent.class, event -> {
-            event.setSpawningInstance(Lobby.INSTANCE);
-            event.getPlayer().setRespawnPoint(Lobby.INSTANCE.getPosition());
-        });
+        MinecraftServer.getGlobalEventHandler()
+                .addListener(PlayerLoginEvent.class, event -> {
+                    event.setSpawningInstance(Lobby.INSTANCE);
+                    event.getPlayer().setRespawnPoint(Lobby.INSTANCE.getPosition());
+                })
+                .addListener(ServerListPingEvent.class, event -> {
+                    ResponseData responseData = event.getResponseData();
+                    Optional<Instance> optionalInstance = Optional.ofNullable(event.getConnection()).map(PlayerConnection::getPlayer).map(Entity::getInstance);
+                    Collection<Player> players;
+                    if (optionalInstance.isPresent()) {
+                        Instance instance = optionalInstance.get();
+                        players = instance.getPlayers();
+                    } else {
+                        players = MinecraftServer.getConnectionManager().getOnlinePlayers();
+                    }
+                    responseData.addEntries(players);
+                    responseData.setMaxPlayer(players.size() + 1);
+                });
 
-        if (MainConfig.BUNGEE.getValue()) {
+        if (Boolean.TRUE.equals(MainConfig.BUNGEE.getValue())) {
             BungeeCordProxy.enable();
         }
         minecraftServer.start(MainConfig.SERVER_IP.getValue(), MainConfig.SERVER_PORT.getValue());
