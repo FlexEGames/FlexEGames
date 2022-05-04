8a651d56f16d8226219f8a067148148f2c1f53f6
HSGamer <huynhqtienvtag@gmail.com>
2022-05-04 10:41:26 +0700

    create new arena

--- src/main/java/me/hsgamer/epicmegagames/manager/GameArenaManager.java
+++ src/main/java/me/hsgamer/epicmegagames/manager/GameArenaManager.java
@@ -1,13 +1,16 @@
 package me.hsgamer.epicmegagames.manager;
 
+import me.hsgamer.epicmegagames.arena.GameArena;
 import me.hsgamer.epicmegagames.feature.TemplateFeature;
 import me.hsgamer.epicmegagames.state.*;
+import me.hsgamer.minigamecore.base.Arena;
 import me.hsgamer.minigamecore.base.ArenaManager;
 import me.hsgamer.minigamecore.base.Feature;
 import me.hsgamer.minigamecore.base.GameState;
 import me.hsgamer.minigamecore.implementation.feature.arena.ArenaTimerFeature;
 
 import java.util.List;
+import java.util.UUID;
 
 public class GameArenaManager extends ArenaManager {
     @Override
@@ -27,5 +30,13 @@ public class GameArenaManager extends ArenaManager {
                 new ArenaTimerFeature(),
                 new TemplateFeature()
         );
+    }
+
+    public Arena createNewArena() {
+        String name;
+        do {
+            name = "Arena-" + UUID.randomUUID();
+        } while (getArenaByName(name).isPresent());
+        return new GameArena(name, this);
     }
 }
