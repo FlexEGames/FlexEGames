72d6d6d017b6ab9f827ee6c6551676f343e1de76
HSGamer <huynhqtienvtag@gmail.com>
2022-05-05 10:46:48 +0700

    optimize imports

--- src/main/java/me/hsgamer/epicmegagames/hook/ServerListHook.java
+++ src/main/java/me/hsgamer/epicmegagames/hook/ServerListHook.java
@@ -1,17 +1,13 @@
 package me.hsgamer.epicmegagames.hook;
 
 import net.minestom.server.MinecraftServer;
-import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.Event;
 import net.minestom.server.event.EventNode;
 import net.minestom.server.event.server.ServerListPingEvent;
-import net.minestom.server.instance.Instance;
-import net.minestom.server.network.player.PlayerConnection;
 import net.minestom.server.ping.ResponseData;
 
 import java.util.Collection;
-import java.util.Optional;
 
 public class ServerListHook {
     public static void hook(EventNode<Event> node) {
