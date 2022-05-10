230caf2572bd8f92f7fc49d9d8b297d52b79579b
HSGamer <huynhqtienvtag@gmail.com>
2022-05-10 11:17:12 +0700

    remove arena synchronously

--- src/main/java/me/hsgamer/epicmegagames/state/KillingState.java
+++ src/main/java/me/hsgamer/epicmegagames/state/KillingState.java
@@ -3,11 +3,12 @@ package me.hsgamer.epicmegagames.state;
 import me.hsgamer.epicmegagames.feature.GameFeature;
 import me.hsgamer.minigamecore.base.Arena;
 import me.hsgamer.minigamecore.base.GameState;
+import net.minestom.server.MinecraftServer;
 
 public class KillingState implements GameState {
     @Override
     public void start(Arena arena) {
         arena.getArenaFeature(GameFeature.class).getGame().clear();
-        arena.removeFromManager();
+        MinecraftServer.getSchedulerManager().scheduleNextTick(arena::removeFromManager);
     }
 }
