14b8a4a26150118c89839049acbd14e84ae62a9b
HSGamer <huynhqtienvtag@gmail.com>
2022-05-10 11:27:17 +0700

    decorate player count

--- src/main/java/me/hsgamer/epicmegagames/command/ListPlayerCommand.java
+++ src/main/java/me/hsgamer/epicmegagames/command/ListPlayerCommand.java
@@ -6,12 +6,17 @@ import net.minestom.server.command.ConsoleSender;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.entity.Player;
 
+import java.util.Collection;
+
 public class ListPlayerCommand extends Command {
     public ListPlayerCommand() {
         super("listplayer", "list");
         setCondition((sender, commandString) -> sender instanceof ConsoleSender);
         setDefaultExecutor((sender, context) -> {
-            Component component = Component.text("Players: ");
+            Collection<Player> players = MinecraftServer.getConnectionManager().getOnlinePlayers();
+            Component component = Component.text("Players ")
+                    .append(Component.text("(" + players.size() + ")"))
+                    .append(Component.text(": "));
             for (Player player : MinecraftServer.getConnectionManager().getOnlinePlayers()) {
                 component = component.append(player.getName()).append(Component.text(", "));
             }
