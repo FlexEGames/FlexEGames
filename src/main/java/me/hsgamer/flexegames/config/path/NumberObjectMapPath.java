678832243922be8c8ee7e5a91ece5ad6ee0ee352
HSGamer <huynhqtienvtag@gmail.com>
2022-05-12 09:21:13 +0700

    NumberObjectMapPath on SerializableMapConfigPath

--- src/main/java/me/hsgamer/flexegames/config/path/NumberObjectMapPath.java
+++ src/main/java/me/hsgamer/flexegames/config/path/NumberObjectMapPath.java
@@ -1,7 +1,6 @@
 package me.hsgamer.flexegames.config.path;
 
-import me.hsgamer.hscore.config.Config;
-import me.hsgamer.hscore.config.path.AdvancedConfigPath;
+import me.hsgamer.hscore.config.path.SerializableMapConfigPath;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -10,43 +9,31 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-public class NumberObjectMapPath extends AdvancedConfigPath<Map<String, Map<String, Object>>, Map<Number, Map<String, Object>>> {
+public class NumberObjectMapPath extends SerializableMapConfigPath<Map<Number, Map<String, Object>>> {
     public NumberObjectMapPath(@NotNull String path, @Nullable Map<Number, Map<String, Object>> def) {
         super(path, def);
     }
 
     @Override
-    public @Nullable Map<String, Map<String, Object>> getFromConfig(@NotNull Config config) {
-        if (!config.contains(getPath())) return null;
-        Map<String, Object> rawMap = config.getNormalizedValues(getPath(), false);
-        Map<String, Map<String, Object>> map = new LinkedHashMap<>();
-        for (Map.Entry<String, Object> entry : rawMap.entrySet()) {
-            Object value = entry.getValue();
-            if (value instanceof Map) {
-                Map<String, Object> mapValue = new LinkedHashMap<>();
-                for (Map.Entry<?, ?> entry1 : ((Map<?, ?>) value).entrySet()) {
-                    mapValue.put(Objects.toString(entry1.getKey()), entry1.getValue());
-                }
-                map.put(entry.getKey(), mapValue);
-            }
-        }
-        return map;
-    }
-
-    @Override
-    public @Nullable Map<Number, Map<String, Object>> convert(@NotNull Map<String, Map<String, Object>> rawValue) {
+    public @Nullable Map<Number, Map<String, Object>> convert(@NotNull Map<String, Object> rawValue) {
         return rawValue.entrySet().stream()
                 .collect(Collectors.toMap(
                         entry -> Double.parseDouble(entry.getKey()),
-                        Map.Entry::getValue,
+                        entry -> {
+                            Map<String, Object> value = new LinkedHashMap<>();
+                            if (entry.getValue() instanceof Map) {
+                                ((Map<?, ?>) entry.getValue()).forEach((key, value1) -> value.put(key.toString(), value1));
+                            }
+                            return value;
+                        },
                         (a, b) -> b,
                         LinkedHashMap::new
                 ));
     }
 
     @Override
-    public @Nullable Map<String, Map<String, Object>> convertToRaw(@NotNull Map<Number, Map<String, Object>> value) {
-        Map<String, Map<String, Object>> map = new LinkedHashMap<>();
+    public @Nullable Map<String, Object> convertToRaw(@NotNull Map<Number, Map<String, Object>> value) {
+        Map<String, Object> map = new LinkedHashMap<>();
         for (Map.Entry<Number, Map<String, Object>> entry : value.entrySet()) {
             map.put(Objects.toString(entry.getKey()), entry.getValue());
         }
