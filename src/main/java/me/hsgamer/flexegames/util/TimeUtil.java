dd641dce52f97254ba9c3f1881bfd5c1e3ae2fbd
HSGamer <huynhqtienvtag@gmail.com>
2022-05-17 22:00:04 +0700

    compact time

--- src/main/java/me/hsgamer/flexegames/util/TimeUtil.java
+++ src/main/java/me/hsgamer/flexegames/util/TimeUtil.java
@@ -7,17 +7,19 @@ import java.time.Duration;
 @UtilityClass
 public final class TimeUtil {
     public static String format(Duration duration) {
-        StringBuilder builder = new StringBuilder();
         int hours = duration.toHoursPart();
         int minutes = duration.toMinutesPart();
         int seconds = duration.toSecondsPart();
 
+        if (hours <= 0 && minutes <= 0) {
+            return Integer.toString(seconds);
+        }
+
+        StringBuilder builder = new StringBuilder();
         if (hours > 0) {
             builder.append(String.format("%02d", hours)).append(":");
         }
-        if (hours > 0 || minutes > 0) {
-            builder.append(String.format("%02d", minutes)).append(":");
-        }
+        builder.append(String.format("%02d", minutes)).append(":");
         builder.append(String.format("%02d", seconds));
         return builder.toString();
     }
