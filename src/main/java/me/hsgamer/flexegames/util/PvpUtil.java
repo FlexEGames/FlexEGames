5a28b023de478b5ae06d8282a04e82fc3d971585
HSGamer <huynhqtienvtag@gmail.com>
2022-05-19 20:32:05 +0700

    update lib & apply explosion

--- src/main/java/me/hsgamer/flexegames/util/PvpUtil.java
+++ src/main/java/me/hsgamer/flexegames/util/PvpUtil.java
@@ -1,13 +1,19 @@
 package me.hsgamer.flexegames.util;
 
 import io.github.bloepiloepi.pvp.PvpExtension;
+import io.github.bloepiloepi.pvp.explosion.PvpExplosionSupplier;
 import lombok.experimental.UtilityClass;
 import net.minestom.server.event.EventNode;
 import net.minestom.server.event.trait.EntityEvent;
+import net.minestom.server.instance.Instance;
 
 @UtilityClass
 public final class PvpUtil {
     public static EventNode<EntityEvent> applyPvp(EventNode<EntityEvent> node, boolean legacy) {
         return node.addChild(legacy ? PvpExtension.legacyEvents() : PvpExtension.events());
+    }
+
+    public static void applyExplosion(Instance instance) {
+        instance.setExplosionSupplier(PvpExplosionSupplier.INSTANCE);
     }
 }
