0795b6452dc79d5e669ac2900ef7dd1c25df341b
HSGamer <huynhqtienvtag@gmail.com>
2022-06-09 14:32:01 +0700

    more aggressive PerInstanceTabListHook

--- main/src/main/java/me/hsgamer/flexegames/hook/PerInstanceTabListHook.java
+++ main/src/main/java/me/hsgamer/flexegames/hook/PerInstanceTabListHook.java
@@ -6,43 +6,22 @@ import net.minestom.server.entity.Player;
 import net.minestom.server.entity.PlayerSkin;
 import net.minestom.server.event.Event;
 import net.minestom.server.event.EventNode;
-import net.minestom.server.event.instance.AddEntityToInstanceEvent;
-import net.minestom.server.event.instance.RemoveEntityFromInstanceEvent;
-import net.minestom.server.instance.Instance;
+import net.minestom.server.event.player.PlayerLoginEvent;
 import net.minestom.server.network.packet.server.play.PlayerInfoPacket;
-import net.minestom.server.utils.PacketUtils;
+import net.minestom.server.timer.ExecutionType;
+import net.minestom.server.timer.TaskSchedule;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @UtilityClass
 public final class PerInstanceTabListHook {
     public static void hook(EventNode<Event> node) {
-        node.addListener(AddEntityToInstanceEvent.class, event -> {
-            if (!(event.getEntity() instanceof Player player)) return;
-            Instance instance = event.getInstance();
-            PacketUtils.sendGroupedPacket(instance.getPlayers(), getAddPlayerPacket(player), player1 -> player1 != player);
-            if (player.getInstance() == null) { // On First Spawn
-                for (Player onlinePlayer : MinecraftServer.getConnectionManager().getOnlinePlayers()) {
-                    if (onlinePlayer == player || onlinePlayer.getInstance() == instance) continue;
-                    PacketUtils.sendPacket(player, getRemovePlayerPacket(onlinePlayer));
-                    PacketUtils.sendPacket(onlinePlayer, getAddPlayerPacket(player));
-                }
-            } else { // On Switch Instance
-                for (Player instancePlayer : instance.getPlayers()) {
-                    if (instancePlayer == player) continue;
-                    PacketUtils.sendPacket(player, getAddPlayerPacket(instancePlayer));
-                }
-            }
-        }).addListener(RemoveEntityFromInstanceEvent.class, event -> {
-            if (!(event.getEntity() instanceof Player player)) return;
-            Instance instance = event.getInstance();
-            PacketUtils.sendGroupedPacket(instance.getPlayers(), getRemovePlayerPacket(player), player1 -> player1 != player);
-            for (Player instancePlayer : instance.getPlayers()) {
-                if (instancePlayer == player) continue;
-                PacketUtils.sendPacket(player, getRemovePlayerPacket(instancePlayer));
-            }
-        });
+        node.addListener(PlayerLoginEvent.class, event -> event.getPlayer().scheduler()
+                .buildTask(new TabPlayerRunnable(event.getPlayer()))
+                .delay(TaskSchedule.tick(5))
+                .repeat(TaskSchedule.tick(5))
+                .executionType(ExecutionType.ASYNC)
+                .schedule());
     }
 
     private static PlayerInfoPacket getAddPlayerPacket(Player player) {
@@ -56,5 +35,43 @@ public final class PerInstanceTabListHook {
 
     private static PlayerInfoPacket getRemovePlayerPacket(Player player) {
         return new PlayerInfoPacket(PlayerInfoPacket.Action.REMOVE_PLAYER, new PlayerInfoPacket.RemovePlayer(player.getUuid()));
+    }
+
+    private static class TabPlayerRunnable implements Runnable {
+        private final Player owner;
+        private final Set<UUID> canSee = new HashSet<>();
+        private final Set<UUID> cannotSee = new HashSet<>();
+
+        private TabPlayerRunnable(Player owner) {
+            this.owner = owner;
+        }
+
+        private boolean isSameInstance(Player player) {
+            return owner.getInstance() == player.getInstance();
+        }
+
+        @Override
+        public void run() {
+            for (Player player : MinecraftServer.getConnectionManager().getOnlinePlayers()) {
+                UUID uuid = player.getUuid();
+                if (canSee.contains(uuid)) {
+                    if (!isSameInstance(player)) {
+                        canSee.remove(uuid);
+                    }
+                } else if (cannotSee.contains(uuid)) {
+                    if (isSameInstance(player)) {
+                        cannotSee.remove(uuid);
+                    }
+                } else {
+                    if (isSameInstance(player)) {
+                        canSee.add(uuid);
+                        owner.sendPacket(getAddPlayerPacket(player));
+                    } else {
+                        cannotSee.add(uuid);
+                        owner.sendPacket(getRemovePlayerPacket(player));
+                    }
+                }
+            }
+        }
     }
 }
