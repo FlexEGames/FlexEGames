af23461cab10c3001c044826878076b4fb0a1300
HSGamer <huynhqtienvtag@gmail.com>
2022-06-10 22:17:02 +0700

    clear all before stopping

--- main/src/main/java/me/hsgamer/flexegames/GameServer.java
+++ main/src/main/java/me/hsgamer/flexegames/GameServer.java
@@ -132,8 +132,8 @@ public class GameServer {
 
     @ApiStatus.Internal
     public void stop() {
-        MinecraftServer.stopCleanly();
         gameArenaManager.clear();
         lobby.clear();
+        MinecraftServer.stopCleanly();
     }
 }
