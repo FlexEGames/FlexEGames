38d3da1af527b8fe551cd0eb9b80d7664ba4031f
HSGamer <huynhqtienvtag@gmail.com>
2023-01-20 18:03:54 +0700

    apply the patch

--- pve/src/main/java/me/hsgamer/flexegames/game/pve/mob/SkeletonMob.java
+++ pve/src/main/java/me/hsgamer/flexegames/game/pve/mob/SkeletonMob.java
@@ -1,9 +1,10 @@
 package me.hsgamer.flexegames.game.pve.mob;
 
+import io.github.bloepiloepi.pvp.projectile.Arrow;
+import me.hsgamer.flexegames.game.pve.feature.ConfigFeature;
 import me.hsgamer.minigamecore.base.Arena;
 import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.Entity;
-import net.minestom.server.entity.EntityProjectile;
 import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.ai.goal.RangedAttackGoal;
 import net.minestom.server.entity.ai.target.ClosestEntityTarget;
@@ -24,10 +25,12 @@ public final class SkeletonMob extends ArenaMob {
                 this, Duration.of(40, TimeUnit.SERVER_TICK),
                 16, 8, false, 1, 0.1);
 
-        rangedAttackGoal.setProjectileGenerator(entity -> {
-            HomingArrow projectile = new HomingArrow(entity, EntityType.PLAYER);
+        var gameConfig = arena.getFeature(ConfigFeature.class).config();
+        rangedAttackGoal.setProjectileGenerator((shooter, target, power, spread) -> {
+            HomingArrow projectile = new HomingArrow(shooter, EntityType.PLAYER, gameConfig.isUseLegacyPvp());
             projectile.scheduleRemove(Duration.of(100, TimeUnit.SERVER_TICK));
-            return projectile;
+            projectile.setInstance(shooter.getInstance(), shooter.getPosition().add(0D, shooter.getEyeHeight(), 0D));
+            projectile.shoot(target, power, spread);
         });
 
         addAIGroup(
@@ -36,11 +39,11 @@ public final class SkeletonMob extends ArenaMob {
         );
     }
 
-    private static final class HomingArrow extends EntityProjectile {
+    private static final class HomingArrow extends Arrow {
         private final EntityType target;
 
-        public HomingArrow(@Nullable Entity shooter, EntityType target) {
-            super(shooter, EntityType.ARROW);
+        public HomingArrow(@Nullable Entity shooter, EntityType target, boolean isLegacy) {
+            super(shooter, isLegacy);
             this.target = target;
         }
 
